d230fb1595e82070ff7262bf7ecbd1bb62219305
Alastair Ong <alastair.ong.a7o@gmail.com>
2026-01-30 11:34:59 +0000

    geoblock

---
+++ src/routes/blocked/+page.svelte
@@ -0,0 +1,11 @@
+<div class="min-h-screen bg-white text-slate-900">
+	<div class="mx-auto flex max-w-5xl flex-col items-center px-4 py-16 md:py-24">
+		<img src="/assets/logo.svg" alt="Albion Labs Logo" class="mb-6 h-24 w-auto" />
+		<h1 class="text-3xl font-extrabold tracking-tight md:text-4xl">
+			Unavailable in Your Region
+		</h1>
+		<p class="mt-4 max-w-lg text-center text-black">
+			This site is not available in your country.
+		</p>
+	</div>
+</div>
